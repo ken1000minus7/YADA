d5d1b7640ad1a22c4564036c73a4cc0e8039598c
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2021-11-26 14:49:44 +0530

    delete account implemented

--- app/src/main/java/com/kamikaze/yada/options/SettingsFragment.java
+++ app/src/main/java/com/kamikaze/yada/options/SettingsFragment.java
@@ -1,14 +1,33 @@
 package com.kamikaze.yada.options;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.kamikaze.yada.MainActivity;
 import com.kamikaze.yada.R;
+import com.kamikaze.yada.diary.DiaryHandler;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +80,129 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view=inflater.inflate(R.layout.fragment_settings, container, false);
+        Button deleteAccount=(Button) view.findViewById(R.id.delete_account_button);
+        Button aboutUs=(Button) view.findViewById(R.id.aboutus_button);
+        Button feedbackButton=(Button) view.findViewById(R.id.feedback_button);
+        deleteAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View dialog=inflater.inflate(R.layout.delete_confirm_dialog,container,false);
+
+                AlertDialog alertDialog=new AlertDialog.Builder(getContext()).setView(dialog).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                        View dialogConfirm=inflater.inflate(R.layout.delete_account_dialog,container,false);
+                        EditText pass=(EditText) dialogConfirm.findViewById(R.id.delete_pass_input);
+                        AlertDialog deleteDialog=new AlertDialog.Builder(getContext()).setView(dialogConfirm).setPositiveButton("Confirm",null).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        }).create();
+                        deleteDialog.getWindow().setBackgroundDrawableResource(R.drawable.dialog_background);
+                        deleteDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                            @Override
+                            public void onShow(DialogInterface dialogInterface) {
+                                Button posButton=(Button) deleteDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                                posButton.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        String password=pass.getText().toString();
+                                        AuthCredential credential= EmailAuthProvider.getCredential(FirebaseAuth.getInstance().getCurrentUser().getEmail(),password);
+                                        FirebaseAuth.getInstance().getCurrentUser().reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if(task.isSuccessful())
+                                                {
+                                                    dialogInterface.cancel();
+                                                    ProgressDialog progressDialog= new ProgressDialog(getContext());
+                                                    progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                                                    progressDialog.setCanceledOnTouchOutside(false);
+                                                    progressDialog.setMessage("Deleting account");
+                                                    progressDialog.getWindow().setBackgroundDrawableResource(R.drawable.empty_list_background);
+                                                    progressDialog.show();
+                                                    String uid= FirebaseAuth.getInstance().getUid();
+                                                    FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
+                                                    Log.d("uid",uid);
+
+                                                    Log.d("uid",uid);
+                                                    Intent intent=new Intent(getActivity(), MainActivity.class);
+                                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                                                    FirebaseFirestore db=FirebaseFirestore.getInstance();
+                                                    DocumentReference documentReference=db.collection("users").document(uid);
+                                                    documentReference.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                        @Override
+                                                        public void onComplete(@NonNull Task<Void> task) {
+
+                                                            if(task.isSuccessful())
+                                                            {
+                                                                FirebaseAuth.getInstance().signOut();
+
+                                                                Log.d("success","deleted");
+                                                                user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                    @Override
+                                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                                        progressDialog.cancel();
+                                                                        if(task.isSuccessful())
+                                                                        {
+                                                                            Toast.makeText(getContext(), "Account deleted", Toast.LENGTH_SHORT).show();
+                                                                            startActivity(intent);
+                                                                            Log.d("uid",uid);
+
+                                                                        }
+                                                                        else
+                                                                        {
+                                                                            Toast.makeText(getContext(), "Failed", Toast.LENGTH_SHORT).show();
+                                                                            startActivity(intent);
+
+                                                                        }
+                                                                    }
+                                                                });
+                                                            }
+                                                            else
+                                                            {
+                                                                Log.d("failed","not deleted");
+                                                                progressDialog.cancel();
+                                                            }
+                                                        }
+                                                    });
+
+                                                }
+                                                else
+                                                    Toast.makeText(getContext(), "Incorrect password", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                        deleteDialog.show();
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                }).create();
+                alertDialog.getWindow().setBackgroundDrawableResource(R.drawable.dialog_background);
+                alertDialog.show();
+            }
+        });
+        aboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "We are good people", Toast.LENGTH_SHORT).show();
+            }
+        });
+        feedbackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        return view;
     }
 }
